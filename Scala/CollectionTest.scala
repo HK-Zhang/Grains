b936b0022fe440ab1d3241f27ef382f8643ed9f9
Zhang HeKe <yxzhk@hotmail.com>
2015-08-14 17:22:42 +0800

    Update CollectionTest.scala
    add map printer

--- Scala/CollectionTest.scala
+++ Scala/CollectionTest.scala
@@ -150,3 +150,39 @@ object BufferOps {
     println(treeMap) //Map(Java -> Hadoop, Scala -> Spark)
   }
 }
+
+object mapPrinter extends App{
+
+  implicit class PrettyPrintMap[K, V](val map: Map[K, V]) {
+    def prettyPrint: PrettyPrintMap[K, V] = this
+
+    override def toString: String = {
+      val valuesString = toStringLines.mkString("\n")
+
+      "Map (\n" + valuesString + "\n)"
+    }
+
+    def toStringLines = {
+      map
+        .flatMap{ case (k, v) => keyValueToString(k, v)}
+        .map(indentLine(_))
+    }
+
+    def keyValueToString(key: K, value: V): Iterable[String] = {
+      value match {
+        case v: Map[_, _] => Iterable(key + " -> Map (") ++ v.prettyPrint.toStringLines ++ Iterable(")")
+        case x => Iterable(key + " -> " + x.toString)
+      }
+    }
+
+    def indentLine(line: String): String = {
+      "\t" + line
+    }
+  }
+
+  type MapType = Map[String, Map[String, Map[String, (String, String)]]]
+
+  val m: MapType = Map("Alphabet" -> Map( "Big Boss" -> Map("Clandestine Mssion" -> ("Dracula Returns", "Enemy at the Gates"))))
+
+  println(m.prettyPrint)
+}
