114716c371681dd2cfc1dd4b99701469554f3015
Zhang HeKe <yxzhk@hotmail.com>
2015-08-17 14:37:22 +0800

    Update Demo.py
    add liner model

--- Python/Demo.py
+++ Python/Demo.py
@@ -3,6 +3,12 @@ def TestVariableScope():
     print(TestVariableScope.a)
     TestVariableScope.a=13
 
+from sklearn import linear_model
+clf = linear_model.LinearRegression()
+clf.fit([[1],[2],[5],[10],[25],[50],[100],[250]],[21.90593302,24.68242796,33.00913072,46.87769512,88.53902951,161.2203224,308.2521436,749.764916])
+clf.coef_
+clf.intercept_
+
 def SetVariable():
     a=12
     TestVariableScope.a=12
