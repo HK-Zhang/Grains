b8a58ed450c93abdfd8e399d27fb7cbf0cc2d1cd
Zhang HeKe <yxzhk@hotmail.com>
2015-09-25 17:16:06 +0800

    Update LoopTest.scala
    Case Class test

--- Scala/LoopTest.scala
+++ Scala/LoopTest.scala
@@ -127,3 +127,50 @@ object NeighborMerge extends App{
   d.foreach(println)
 
 }
+
+object CaseClassTesting extends App {
+
+  abstract class Term
+
+  case class Var(name: String) extends Term
+
+  case class Fun(arg: String, body: Term) extends Term
+
+  case class App(f: Term, v: Term) extends Term
+
+  val x1 = Var("x")
+  val x2 = Var("x")
+  val y1 = Var("y")
+  println("" + x1 + " == " + x2 + " => " + (x1 == x2))
+  println("" + x1 + " == " + y1 + " => " + (x1 == y1))
+
+  val x = Var("x")
+  Console.println(x.name)
+
+  def printTerm(term: Term) {
+    term match {
+      case Var(n) =>
+        print(n)
+      case Fun(x, b) =>
+        print("^" + x + ".")
+        printTerm(b)
+      case App(f, v) =>
+        Console.print("(")
+        printTerm(f)
+        print(" ")
+        printTerm(v)
+        print(")")
+    }
+  }
+  def isIdentityFun(term: Term): Boolean = term match {
+    case Fun(x, Var(y)) if x == y => true
+    case _ => false
+  }
+  val id = Fun("x", Var("x"))
+  val t = Fun("x", Fun("y", App(Var("x"), Var("y"))))
+  printTerm(t)
+  println
+  println(isIdentityFun(id))
+  println(isIdentityFun(t))
+
+}
