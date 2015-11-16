5b4a7c359408bd06466cda43a5e370e78f81d727
Zhang HeKe <yxzhk@hotmail.com>
2015-11-16 17:46:49 +0800

    Update ArrayOps.cpp

--- CPP/Seed/Seed/ArrayOps.cpp
+++ CPP/Seed/Seed/ArrayOps.cpp
@@ -1,39 +1,45 @@
 #include <iostream>
 #include <stdio.h>
 
-int calInx(int i,int pace,int len)
+int reverseArray(char* source,int l)
 {
-	int t=(i+pace)%len;
-	return t;
-	//if(t<len)
-	//{
-	//	return t;
-	//}
-	//else
-	//{
-	//	return t-len;
-	//}
+	char temp;
+
+	for(int i=0;i<l/2;i++)
+	{
+		temp = source[i];
+		source[i] = source[l-i-1];
+		source[l-i-1] = temp;
+	}
+
+	return 0;
+}
+
+int rotateArray2(char* source,int k,int l)
+{
+	reverseArray(source,k);
+	reverseArray(&source[k],l-k);
+	reverseArray(source,l);
+	return 0;
 }
 
 int rotateArray(char* source,int k,int l)
 {
-	//char source[11]="abcdefhigk";
 
+	char b;
+	int kb;
+	int kn;
 
-	//int k=3;
-	//int l=sizeof(source)-1;
-
-	char b=source[0];
-	int kb=0;
-	int kn=(k)%l;
 	int m=(l%k == 0 ? k : 1);
 
+	//if array's length can be divided by k then it is need to loop k round. otherwise loop 1 round is enough.
 	for(int i=0;i<m;i++)
 	{
 		b=source[i];
 		kb=i;
-		kn=(k+i)%l;
+		kn=(k+i)%l;//caculate the index of value which will fullfill the current slot located at index i.
 
+		//ended when next slot is the one which the first slot to fullfill.
 		while(kn!=l-k+i)
 		{
 			source[kb]=source[kn];
@@ -45,24 +51,25 @@ int rotateArray(char* source,int k,int l)
 		source[kn]=b;
 	}
 
+	return 0;
+}
 
+int maxSerial(int* s,int l)
+{
+	int f=0;
+	int maxL=1;
+	int itv=1;
+	int tl=1;
 
-	//while(kn!=l-k)
-	//{
-	//	source[kb]=source[kn];
-	//	kb=kn;
-	//	kn=(kb+k)%l;
-	//}
+	for(int i=0;i<l;i++)
+	{
+		if(itv==s[i]-s[i+1])
+		{
+			tl=tl+1;
 
-	//source[kb]=source[kn];
-	//source[kn]=b;
+		}
+	}
 
-
-
-	//m=calInx(0,3,l);
-	//char c[20];
-	//sprintf(c,"%d",m);
-	//printf(c);
 
 	return 0;
 }
