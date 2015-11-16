be999e41702719056eb9ffc7119aca5e3310c66a
Zhang HeKe <yxzhk@hotmail.com>
2015-11-16 17:47:48 +0800

    Create Main.h

---
+++ CPP/Seed/Seed/Main.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <iostream>
+#include <bitset>
+#include "SineGraph.h"
+#include "ArrayOps.h"
+
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#define PRINT_STR "Hello World!!!"
+#endif
