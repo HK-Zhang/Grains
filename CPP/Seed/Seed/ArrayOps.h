b3a3eabf24a068ac43689c3970a65e879f4226a1
Zhang HeKe <yxzhk@hotmail.com>
2015-11-16 17:46:32 +0800

    Update ArrayOps.h

--- CPP/Seed/Seed/ArrayOps.h
+++ CPP/Seed/Seed/ArrayOps.h
@@ -1,1 +1,8 @@
+#ifndef _ARRAY_OPS_H_
+#define _ARRAY_OPS_H_
+
 int rotateArray(char* source,int k,int l);
+int reverseArray(char* source,int l);
+int rotateArray2(char* source,int k,int l);
+
+#endif
