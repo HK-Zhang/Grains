4c49464ab969d14a1c5bb2ae421df07631665eb9
Zhang HeKe <yxzhk@hotmail.com>
2015-12-09 16:33:40 +0800

    Create Sort.h

---
+++ CPP/Seed/Seed/Sort.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+
+#ifndef _SORT_H_
+#define _SORT_H_
+#define random(x)(rand()%x)
+int bubbleSort(int a[],int n);
+int bubbleSortSimple(int a[],int n);
+int sortTest();
+//int randomTest2(vector<int> c);
+#endif
