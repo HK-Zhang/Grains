5dcd3726ccf7be10813148e4de799daee1057fe7
Zhang HeKe <yxzhk@hotmail.com>
2015-12-15 16:55:48 +0800

    Update PointOps.cpp

--- CPP/Seed/Seed/PointOps.cpp
+++ CPP/Seed/Seed/PointOps.cpp
@@ -14,3 +14,38 @@ int pointTrial()
 
 	return 0;
 }
+
+char *GetString(void)
+{
+	char p[] = "hello world";
+	cout<<&p<<" "<<p<<endl;
+	return p; // 编译器将提出警告
+	
+}
+
+void pointTrial3(void)
+{
+	char *str = NULL;
+	str = GetString(); // str 的内容是垃圾
+	cout<< str << endl;
+}
+
+
+int pointTrial2()
+{
+	int a[5]={1,2,3,4,5};
+	int *p=a;
+	cout<<&p<<" "<<p<<" "<<&a<<" "<<a<<endl;
+	return 0;
+}
+
+int pointTrial4()
+{
+	int a=0;
+	int *p=&a;
+	cout<<&p<<" "<<p<<" "<<&a<<" "<<a<<" "<<*p<<endl;
+	int **p1=&p;
+	cout<<&p1<<" "<<p1<<" "<<*p1<<" "<<**p1<<endl;
+	//int *p2=p1;
+	return 0;
+}
