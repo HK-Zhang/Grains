cc68afe389f0f1c7a32d6cee05fbfc53585643d7
Zhang HeKe <yxzhk@hotmail.com>
2015-12-16 17:02:17 +0800

    Create ExternTest1.h

---
+++ CPP/Seed/Seed/ExternTest1.h
@@ -0,0 +1,9 @@
+#ifndef _EXTERN_TEST_H_
+#define _EXTERN_TEST_H_
+#include <iostream>
+extern char g_str[];
+static char s_str[]="654321";
+//char h_str[]="654321";
+//extern char h_str[]="654321";
+void externTestFun1();
+#endif
