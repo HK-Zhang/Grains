1c2ba513dae0c4a8b19026705133d2e2c3da9e43
HK Zhang <yxzhk@hotmail.com>
2015-12-30 19:34:55 +0800

    luckyDraw

---
+++ CSharp/LuckyDraw/LuckyDraw/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace LuckyDraw
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLucky = new System.Windows.Forms.Label();
+            this.btnControl = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblLucky
+            // 
+            this.lblLucky.AutoSize = true;
+            this.lblLucky.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblLucky.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLucky.Location = new System.Drawing.Point(0, 0);
+            this.lblLucky.Name = "lblLucky";
+            this.lblLucky.Size = new System.Drawing.Size(0, 108);
+            this.lblLucky.TabIndex = 0;
+            // 
+            // btnControl
+            // 
+            this.btnControl.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnControl.Location = new System.Drawing.Point(0, 239);
+            this.btnControl.Name = "btnControl";
+            this.btnControl.Size = new System.Drawing.Size(284, 23);
+            this.btnControl.TabIndex = 1;
+            this.btnControl.Text = "START";
+            this.btnControl.UseVisualStyleBackColor = true;
+            this.btnControl.Click += new System.EventHandler(this.btnControl_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnControl);
+            this.Controls.Add(this.lblLucky);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lucky Draw";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLucky;
+        private System.Windows.Forms.Button btnControl;
+    }
+}
+
---
+++ CSharp/LuckyDraw/LuckyDraw/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LuckyDraw
+{
+    public partial class Form1 : Form
+    {
+        private List<string> EmpLst = new List<string>();
+        private List<string> luckerLst = new List<string>();
+        Predicate<string> prid;
+        Action<object> act;
+        bool flgStop = false;
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnControl_Click(object sender, EventArgs e)
+        {
+            if (btnControl.Text == "STOP")
+            {
+                funStop();
+            }
+            else 
+            {
+                funStart();
+            }
+            
+        }
+
+        private void funStart() 
+        {
+            btnControl.Text = "STOP";
+            flgStop = false;
+
+            SynchronizationContext uiContext = SynchronizationContext.Current;
+            Thread thread = new Thread(run);
+            thread.Start(uiContext);
+            
+
+        }
+
+        private int returnRandom()
+        {
+            int size = EmpLst.Count;
+            Random rad = new Random();
+            return rad.Next(0, size - 1);
+        }
+
+        private void funStop()
+        {
+            flgStop = true;
+
+            int RanKey = returnRandom();
+            lblLucky.Text = EmpLst[RanKey];
+            
+            outPutResult(EmpLst[RanKey]);
+            luckerLst.Add(EmpLst[RanKey]);
+            EmpLst.Remove(EmpLst[RanKey]);
+
+            btnControl.Text = "START";
+
+        }
+
+        private void outPutResult(string cnt) 
+        {
+            StreamWriter sw = new StreamWriter("lucky.csv", true);
+            try
+            {
+                sw.WriteLine(cnt);
+            }
+            finally
+            {
+                sw.Close();
+            }
+
+        }
+
+        private void loadList() 
+        {
+            StreamReader sr = new StreamReader("list.csv", Encoding.Default);
+
+            try
+            {
+                String line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    EmpLst.Add(line);
+                }
+            }
+            finally
+            {
+                sr.Close();
+            }
+
+            sr = new StreamReader("lucky.csv", Encoding.Default);
+            try
+            {
+                String line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    luckerLst.Add(line);
+                }
+            }
+            finally
+            {
+                sr.Close();
+            }
+
+        }
+
+        private void initialize() 
+        {
+            loadList();
+            EmpLst.RemoveAll(prid);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            prid = delegate(string cur)
+            {
+                return luckerLst.Contains(cur);
+            };
+
+            act = str => { lblLucky.Text = str as string; };
+       
+
+            initialize();
+
+        }
+
+        private void run(object state)
+        {
+            SynchronizationContext uiContext = state as SynchronizationContext;
+
+            while (!flgStop)
+            {
+                //uiContext.Post(updateUI, EmpLst[returnRandom()]);
+                uiContext.Post(par => { act(par); }, EmpLst[returnRandom()]);
+
+                Thread.Sleep(100);
+            }
+        }
+
+        private void updateUI(object state) 
+        {
+            act(state);
+        }
+    }
+}
