e8d5a3bafd5b8da15aef9e3ab0d3d565f54e4b4d
Zhang HeKe <yxzhk@hotmail.com>
2016-01-18 16:59:18 +0800

    Create SingletonB.java

---
+++ Java/Demo/src/dp/singleton/SingletonB.java
@@ -0,0 +1,27 @@
+package dp.singleton;
+
+public class SingletonB {
+	private static SingletonB instance;
+	
+	private SingletonB() {
+
+	}
+	
+	private static synchronized void syncInit() {
+		instance = new SingletonB();
+	}
+	
+	public static SingletonB GetInstance() {
+		if(instance == null)
+		{
+			syncInit();
+		}
+		
+		return instance;
+	}
+	
+	public void Action() {
+		System.out.println("Action is completed");
+	}
+
+}
