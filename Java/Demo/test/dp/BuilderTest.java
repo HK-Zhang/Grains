c62252580032f8fa57599a4c05fe09ceee1c49d0
Zhang HeKe <yxzhk@hotmail.com>
2016-01-19 11:57:51 +0800

    Create BuilderTest.java

---
+++ Java/Demo/test/dp/BuilderTest.java
@@ -0,0 +1,13 @@
+package dp;
+
+import dp.builder.*;
+
+public class BuilderTest {
+
+	public static void main(String[] args) {
+		Builder builder = new Builder();
+		builder.produceMailSender(10);
+
+	}
+
+}
