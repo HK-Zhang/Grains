420f7209e5eb107ec57e48e9b0970c05d380e0d8
Zhang HeKe <yxzhk@hotmail.com>
2016-01-20 13:43:48 +0800

    Create AdaptorViaCls.java

---
+++ Java/Demo/src/dp/adaptor/AdaptorViaCls.java
@@ -0,0 +1,11 @@
+package dp.adaptor;
+
+public class AdaptorViaCls extends Source implements ITargetable {
+
+	@Override
+	public void method2() {
+		System.out.println("This is the targetable method");
+
+	}
+
+}
