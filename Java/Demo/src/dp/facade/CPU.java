96fc5db603ca10165174fe5639fd8b66f5e3acfd
Zhang HeKe <yxzhk@hotmail.com>
2016-01-21 14:50:20 +0800

    Create CPU.java

---
+++ Java/Demo/src/dp/facade/CPU.java
@@ -0,0 +1,12 @@
+package dp.facade;
+
+public class CPU {
+	public void StartUp() {
+		System.out.println("CPU startup");
+	}
+	
+	public void ShutDown() {
+		System.out.println("CPU shutdown");
+	}
+
+}
