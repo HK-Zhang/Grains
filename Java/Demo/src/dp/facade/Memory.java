9cfe866390c3e4e00ff5c4dbbdfd401481b46bdd
Zhang HeKe <yxzhk@hotmail.com>
2016-01-21 14:50:50 +0800

    Create Memory.java

---
+++ Java/Demo/src/dp/facade/Memory.java
@@ -0,0 +1,11 @@
+package dp.facade;
+
+public class Memory {
+	public void StartUp() {
+		System.out.println("Memory startup");
+	}
+	
+	public void ShutDown() {
+		System.out.println("Memory shutdown");
+	}
+}
