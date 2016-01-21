244fe6b81fe7a14313343596e6d8a460dffcf86e
Zhang HeKe <yxzhk@hotmail.com>
2016-01-21 14:50:37 +0800

    Create Disk.java

---
+++ Java/Demo/src/dp/facade/Disk.java
@@ -0,0 +1,11 @@
+package dp.facade;
+
+public class Disk {
+	public void StartUp() {
+		System.out.println("Disk startup");
+	}
+	
+	public void ShutDown() {
+		System.out.println("Disk shutdown");
+	}
+}
