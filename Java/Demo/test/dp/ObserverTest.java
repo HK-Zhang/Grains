ea5788004db0303a1b3937d778053fdbc23cb581
Zhang HeKe <yxzhk@hotmail.com>
2016-01-26 11:50:27 +0800

    Create ObserverTest.java

---
+++ Java/Demo/test/dp/ObserverTest.java
@@ -0,0 +1,15 @@
+package dp;
+
+import dp.observer.*;
+
+public class ObserverTest {
+
+	public static void main(String[] args) {
+		ISubject subject= new MySubject();
+		subject.add(new Observer1());
+		subject.add(new Observer2());
+		subject.operation();
+
+	}
+
+}
