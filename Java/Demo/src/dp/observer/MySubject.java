f34d1873cc9ce29e6772e572f4e31b2a54346184
Zhang HeKe <yxzhk@hotmail.com>
2016-01-26 11:49:15 +0800

    Create MySubject.java

---
+++ Java/Demo/src/dp/observer/MySubject.java
@@ -0,0 +1,12 @@
+package dp.observer;
+
+public class MySubject extends AbstractSubject {
+
+	@Override
+	public void operation() {
+		 System.out.println("update self!");  
+		 notifyObservers();
+
+	}
+
+}
