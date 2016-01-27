a21cffc989eff132dc803a7c83af099e006eb7bc
Zhang HeKe <yxzhk@hotmail.com>
2016-01-27 12:35:42 +0800

    Create chainResponsibilityTest.java

---
+++ Java/Demo/test/dp/chainResponsibilityTest.java
@@ -0,0 +1,19 @@
+package dp;
+import dp.chainResponsibility.*;
+
+public class chainResponsibilityTest {
+
+	public static void main(String[] args) {
+		MyHandler h1 = new MyHandler("h1");
+		MyHandler h2 = new MyHandler("h2");
+		MyHandler h3 = new MyHandler("h3");
+		
+		h1.setHandler(h2);
+		h2.setHandler(h3);
+		
+		h1.operate();
+		
+
+	}
+
+}
