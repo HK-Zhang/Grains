0b6238861b527281324008bbef18ed4bf4000359
Zhang HeKe <yxzhk@hotmail.com>
2016-02-01 13:34:30 +0800

    Create User2.java

---
+++ Java/Demo/src/dp/mediator/User2.java
@@ -0,0 +1,16 @@
+package dp.mediator;
+
+public class User2 extends User {
+
+	public User2(IMediator value) {
+		super(value);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void work() {
+		System.out.println("User2 exe");
+
+	}
+
+}
