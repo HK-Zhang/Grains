8c18f000ccb7bd1466a76a618811ef565d4961ac
Zhang HeKe <yxzhk@hotmail.com>
2016-02-04 11:23:00 +0800

    Create User.java

---
+++ Java/Demo/src/basic/magic/spring/po/User.java
@@ -0,0 +1,30 @@
+package magic.spring.po;
+
+public class User {
+
+	private String name;
+	private int age;
+	private int score;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	public String toString(){
+		return "tostring from user";
+	}
+}
