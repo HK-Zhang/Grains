7e74d2b6866d078a9c265e5e85c92bc7242ff63b
HK Zhang <yxzhk@hotmail.com>
2016-03-13 17:28:30 +0800

    Type List

---
+++ CSharp/CsPoc/CsPoc/Collection/TypeListDemo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsPoc
+{
+    public class TypeListDemo
+    {
+        public void Execute()
+        {
+            //Foo1();
+            Foo3();
+        }
+
+        public void Foo1()
+        {
+            IList<TypeParent> lt = new List<TypeParent>();
+            lt.Add(new TypeParent { strP = "parent" });
+            lt.Add(new TypeChild { strP = "child" });
+            lt.ToList().ForEach((t) => { Console.WriteLine(t.strP); });
+
+        }
+
+        public void Foo2()
+        {
+            IList<TypeChild> lt = new List<TypeChild>();
+            //lt.Add(new TypeParent { strP = "parent" });
+            lt.Add(new TypeChild { strP = "child" });
+            lt.ToList().ForEach((t) => { Console.WriteLine(t.strP); });
+        }
+
+        public void Foo3()
+        {
+            ITypeList<TypeParent> lt = new TypeList<TypeParent>();
+            lt.Add<TypeParent>();
+            lt.Add(typeof(TypeChild));
+            lt.Add(typeof(int));
+            //lt.Add(new TypeParent { strP = "parent" });
+            //lt.Add(new TypeChild { strP = "child" });
+            lt.ToList().ForEach((t) => { Console.WriteLine(t.ToString()); });
+        }
+
+    }
+
+    public class TypeParent
+    {
+        public string strP { get; set;}
+    }
+
+    public class TypeChild : TypeParent 
+    {
+        public string strC { get; set; }
+    }
+}
