64b8fbebd8d5270b1bd16f67e580a6501e868b23
Zhang HeKe <yxzhk@hotmail.com>
2016-03-22 13:32:04 +0800

    Create TimerDemo.cs

---
+++ CSharp/CsPoc/CsPoc/Basic/TimerDemo.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSDemo
+{
+    public class TimerDemo
+    {
+        public static void Execute() 
+        {
+            TimerObject s = new TimerObject();
+            TimerCallback timerdelegate = new TimerCallback(CheckStatus);
+            Timer timer = new Timer(timerdelegate, s, 2000, 1000);
+
+            while (s.Counter < 10)
+            {
+                Thread.Sleep(1);
+            }
+
+            timer.Change(1000, 500);
+            Console.WriteLine("Timer speed up！");
+
+            while (s.Counter < 20)
+            {
+                Thread.Sleep(1);
+            }
+
+            timer.Dispose();
+            Console.WriteLine("Timer over！");
+            Console.ReadLine();
+        }
+
+
+
+
+        static void CheckStatus(object state)
+        {
+            TimerObject s = (TimerObject)state;
+            s.Counter++;
+            Console.WriteLine("current time：{0} timer count：{1}", DateTime.Now.ToString(), s.Counter);
+
+        }
+    }
+
+    class TimerObject
+    {
+        public int Counter = 0;
+    }
+}
