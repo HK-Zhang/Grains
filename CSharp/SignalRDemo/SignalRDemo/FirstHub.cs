81bcaea4a001940eaf6688f18f0132b0cdfe6d16
HK Zhang <yxzhk@hotmail.com>
2016-03-24 17:58:38 +0800

    SignalR

---
+++ CSharp/SignalRDemo/SignalRDemo/FirstHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalRDemo
+{
+    //Hub的别名,方便前台调用
+    [HubName("getMessage")]
+    public class FirstHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        //编写发送信息的方法
+        public void SendMessage(string message)
+        {
+            //调用所有客户注册的本地的JS方法(broadcastMessage)
+            Clients.All.broadcastMessage(message + DateTime.Now.ToString());
+        } 
+
+    }
+}
---
+++ CSharp/SignalRDemo/SignalRDemo/MyStartup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalRDemo.MyStartup))]
+
+namespace SignalRDemo
+{
+    public class MyStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            //注册管道,使用默认的虚拟地址,根目录下的"/signalr",当然你也可以自己定义
+            app.MapSignalR();
+        }
+    }
+}
