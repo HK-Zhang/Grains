6b777af99a4df93094feb0eb55cf28f39d094704
Zhang HeKe <yxzhk@hotmail.com>
2016-03-30 16:08:21 +0800

    Update CommandRunner.cs

--- CsUnit/CSLibrary/CommandRunner.cs
+++ CsUnit/CSLibrary/CommandRunner.cs
@@ -9,6 +9,7 @@ namespace LiveTDD
     public interface ICommand : IDisposable
     {
         void Execute();
+        event EventHandler Executed;
     }
 
     public class CommandRunner
@@ -27,6 +28,18 @@ namespace LiveTDD
         }
     }
 
+    public class SomethingThatNeedsACommand
+    {
+        ICommand command;
+        public SomethingThatNeedsACommand(ICommand command)
+        {
+            this.command = command;
+        }
+        public void DoSomething() { command.Execute(); }
+        public void DontDoAnything() { }
+    }
+
+
     public class SomeClassWithCtorArgs : IDisposable
     {
         public SomeClassWithCtorArgs(int arg1, string arg2)
@@ -34,5 +47,51 @@ namespace LiveTDD
         }
 
         public void Dispose() { }
+    }
+
+    public class CommandRepeater
+    {
+        ICommand command;
+        int numberOfTimesToCall;
+        public CommandRepeater(ICommand command, int numberOfTimesToCall)
+        {
+            this.command = command;
+            this.numberOfTimesToCall = numberOfTimesToCall;
+        }
+
+        public void Execute()
+        {
+            for (var i = 0; i < numberOfTimesToCall; i++) command.Execute();
+        }
+    }
+
+    public class CommandWatcher
+    {
+        ICommand command;
+        public CommandWatcher(ICommand command)
+        {
+            this.command = command;
+            this.command.Executed += OnExecuted;
+        }
+        public bool DidStuff { get; private set; }
+        public void OnExecuted(object o, EventArgs e)
+        {
+            DidStuff = true;
+        }
+    }
+
+    public class OnceOffCommandRunner
+    {
+        ICommand command;
+        public OnceOffCommandRunner(ICommand command)
+        {
+            this.command = command;
+        }
+        public void Run()
+        {
+            if (command == null) return;
+            command.Execute();
+            command = null;
+        }
     }
 }
