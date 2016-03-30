7da5089877e81922896da94ac378188e57fc6ce8
Zhang HeKe <yxzhk@hotmail.com>
2016-03-30 16:07:57 +0800

    Update ICalculator.cs

--- CsUnit/CSLibrary/ICalculator.cs
+++ CsUnit/CSLibrary/ICalculator.cs
@@ -1,10 +1,12 @@
-﻿using System;
-namespace CSLibrary
+using System;
+namespace LiveTDD
 {
     public interface ICalculator
     {
         int Add(int a, int b);
+        int Subtract(int a, int b);
         string Mode { get; set; }
         event EventHandler PoweringUp;
     }
 }
+
