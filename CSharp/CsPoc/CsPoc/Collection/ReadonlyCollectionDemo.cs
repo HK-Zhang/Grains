b0e50b0e557201320e56d673c890af27314d088b
HK Zhang <yxzhk@hotmail.com>
2016-04-01 14:16:52 +0800

    ThreadSafeDictionary

---
+++ CSharp/CsPoc/CsPoc/Collection/ReadonlyCollectionDemo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsPoc.Collection
+{
+    public class ReadonlyCollectionDemo
+    {
+        public void Execute()
+        {
+            //FooReadOnlyList();
+            FooIMutableList();
+
+            //FooReadonlyListflaw();
+        }
+
+        private void FooReadOnlyList()
+        {
+            IReadOnlyList<string> lst = new List<string> {"a","b","c" };
+            lst.ToList().ForEach((s) => Console.WriteLine(s));
+            lst = new List<string> { "d", "b", "c" };
+            lst.ToList().ForEach((s) => Console.WriteLine(s));
+        }
+
+        private void FooReadonlyListflaw()
+        {
+            List<string> lst2 = new List<string> { "a", "b", "c", "d" };
+            IReadOnlyList<string> lst = lst2;
+            lst.ToList().ForEach((s) => Console.WriteLine(s));
+
+            lst2.Add("e");
+            lst.ToList().ForEach((s) => Console.WriteLine(s));
+        }
+
+        private void FooIMutableList()
+        {
+            var color1 = ImmutableList.Create("orange", "red", "blue");
+            color1.ForEach((s) => Console.WriteLine(s));
+
+             List<string> lst2 = new List<string> { "a", "b", "c", "d" };
+             var lst = lst2.ToImmutableList();
+             lst.ForEach((s) => Console.WriteLine(s));
+
+             lst2.Add("e");
+             lst.ForEach((s) => Console.WriteLine(s));
+
+
+
+        }
+    }
+}
---
+++ CSharp/CsPoc/CsPoc/Collection/ThreadSafeDicDemo.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CsPoc.Collection
+{
+    public class ThreadSafeDicDemo
+    {
+        public void Execute()
+        {
+            Foo1();
+            Foo2();
+            Foo3();
+            Foo4();
+        }
+
+        private void Foo1()
+        {
+            var concurentDictionary = new ConcurrentDictionary<int, int>();
+
+            var w = new ManualResetEvent(false);
+            int timedCalled = 0;
+            var threads = new List<Thread>();
+            for (int i = 0; i < Environment.ProcessorCount; i++)
+            {
+                threads.Add(new Thread(() =>
+                {
+                    w.WaitOne();
+                    concurentDictionary.GetOrAdd(1, i1 =>
+                    {
+                        Interlocked.Increment(ref timedCalled);
+                        return 1;
+                    });
+                }));
+                threads.Last().Start();
+            }
+
+            w.Set();//release all threads to start at the same time         
+            Thread.Sleep(100);
+            Console.WriteLine(timedCalled);// output is 4, means call initial 4 times
+            //Console.WriteLine(concurentDictionary.Keys.Count);
+
+        }
+
+        private void Foo2()
+        {
+            var concurentDictionary = new ConcurrentDictionary<int, int>();
+
+            var w = new ManualResetEvent(false);
+            int timedCalled = 0;
+            var threads = new List<Thread>();
+            Lazy<int> lazy = new Lazy<int>(() => { Interlocked.Increment(ref timedCalled); return 1; });
+            for (int i = 0; i < Environment.ProcessorCount; i++)
+            {
+                threads.Add(new Thread(() =>
+                {
+                    w.WaitOne();
+                    concurentDictionary.GetOrAdd(1, i1 =>
+                    {
+                        return lazy.Value;
+                    });
+                }));
+                threads.Last().Start();
+            }
+
+            w.Set();//release all threads to start at the same time         
+            Thread.Sleep(100);
+            Console.WriteLine(timedCalled);// output is 1
+
+        }
+
+        private void Foo3()
+        {
+            var concurentDictionary = new Dictionary<int, int>();
+            var rwLockSlim = new ReaderWriterLockSlim();
+
+            var w = new ManualResetEvent(false);
+            int timedCalled = 0;
+            var threads = new List<Thread>();
+            int j;
+            Lazy<int> lazy = new Lazy<int>(() => { Interlocked.Increment(ref timedCalled); return 1; });
+            for (int i = 0; i < Environment.ProcessorCount; i++)
+            {
+                threads.Add(new Thread(() =>
+                {
+                    w.WaitOne();
+                    rwLockSlim.EnterUpgradeableReadLock();
+                    try
+                    {
+                        if (!concurentDictionary.TryGetValue(1, out j))
+                        {
+                            rwLockSlim.EnterWriteLock();
+                            try
+                            {
+                                Interlocked.Increment(ref timedCalled);
+                                concurentDictionary[1] = 1;
+                            }
+                            finally
+                            {
+                                rwLockSlim.ExitWriteLock();
+                            }
+                        }
+                    }
+                    finally 
+                    {
+                        rwLockSlim.ExitUpgradeableReadLock();
+
+                    }
+                }));
+
+                threads.Last().Start();
+            }
+
+            w.Set();//release all threads to start at the same time         
+            Thread.Sleep(100);
+            Console.WriteLine(timedCalled);// output is 1
+
+        }
+
+        private void Foo4()
+        {
+            var concurentDictionary = new Dictionary<int, int>();
+            var rwLockSlim = new ReaderWriterLockSlim();
+
+            var w = new ManualResetEvent(false);
+            int timedCalled = 0;
+            var threads = new List<Thread>();
+            int j;
+            Lazy<int> lazy = new Lazy<int>(() => { Interlocked.Increment(ref timedCalled); return 1; });
+            for (int i = 0; i < Environment.ProcessorCount; i++)
+            {
+                threads.Add(new Thread(() =>
+                {
+                    w.WaitOne();
+                    bool exist = false;
+                    rwLockSlim.EnterReadLock();
+                    try 
+                    {
+                        exist = concurentDictionary.TryGetValue(1, out j);
+                    }
+                    finally 
+                    {
+                        rwLockSlim.ExitReadLock();
+                    }
+
+                    if (!exist)
+                    {
+                        rwLockSlim.EnterUpgradeableReadLock();
+                        try
+                        {
+                            if (!concurentDictionary.TryGetValue(1, out j))
+                            {
+                                rwLockSlim.EnterWriteLock();
+                                try
+                                {
+                                    Interlocked.Increment(ref timedCalled);
+                                    concurentDictionary[1] = 1;
+                                }
+                                finally
+                                {
+                                    rwLockSlim.ExitWriteLock();
+                                }
+                            }
+                        }
+                        finally
+                        {
+                            rwLockSlim.ExitUpgradeableReadLock();
+
+                        }
+ 
+                    }
+
+
+                }));
+
+                threads.Last().Start();
+            }
+
+            w.Set();//release all threads to start at the same time         
+            Thread.Sleep(100);
+            Console.WriteLine(timedCalled);// output is 1
+
+        }
+    }
+}
