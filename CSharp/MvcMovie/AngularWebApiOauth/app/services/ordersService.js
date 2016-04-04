1a993ca1b97f524c39e2d8a0ec27146f7792e38b
HK Zhang <yxzhk@hotmail.com>
2016-04-04 10:25:08 +0800

    AngularWebApiOauth

---
+++ CSharp/MvcMovie/AngularWebApiOauth/app/services/ordersService.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+app.factory('ordersService', ['$http', function ($http) {
+
+    var serviceBase = 'http://localhost:42426/';
+    var ordersServiceFactory = {};
+
+    var _getOrders = function () {
+
+        return $http.get(serviceBase + 'api/orders').then(function (results) {
+            return results;
+        });
+    };
+
+    ordersServiceFactory.getOrders = _getOrders;
+
+    return ordersServiceFactory;
+
+}]);
