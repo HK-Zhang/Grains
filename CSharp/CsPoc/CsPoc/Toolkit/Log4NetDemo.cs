e14704df5456fa8bb8e8ab18c429e3bf1fa46323
Zhang HeKe <yxzhk@hotmail.com>
2016-04-12 16:18:26 +0800

    Create Log4NetDemo.cs

---
+++ CSharp/CsPoc/CsPoc/Toolkit/Log4NetDemo.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSDemo
+{
+    public class Log4NetDemo
+    {
+        public void Execute()
+        {
+            LogHelper.SetConfig(new System.IO.FileInfo("log4net.config"));
+            LogHelper.WriteLog("App start");
+
+
+            char c = '\uDC1B';
+
+            String a = c.ToString();
+            Console.WriteLine(c);
+        }
+    }
+}
