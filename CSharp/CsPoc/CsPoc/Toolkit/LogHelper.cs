dab85b3028ddcace3b633ea55cb240f456d3d001
Zhang HeKe <yxzhk@hotmail.com>
2016-04-12 16:18:12 +0800

    Create LogHelper.cs

---
+++ CSharp/CsPoc/CsPoc/Toolkit/LogHelper.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+namespace CSDemo
+{
+    public class LogHelper
+    {
+        //log4net日志专用
+        public static readonly log4net.ILog loginfo = log4net.LogManager.GetLogger("loginfo");
+        public static readonly log4net.ILog logerror = log4net.LogManager.GetLogger("logerror");
+
+        public static void SetConfig()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+        }
+
+        public static void SetConfig(FileInfo configFile)
+        {
+            log4net.Config.XmlConfigurator.Configure(configFile);
+        }
+        /// <summary>
+        /// 普通的文件记录日志
+        /// </summary>
+        /// <param name="info"></param>
+        public static void WriteLog(string info)
+        {
+            if (loginfo.IsInfoEnabled)
+            {
+                loginfo.Info(info);
+            }
+        }
+        /// <summary>
+        /// 错误日志
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="se"></param>
+        public static void WriteLog(string info, Exception se)
+        {
+            if (logerror.IsErrorEnabled)
+            {
+                logerror.Error(info, se);
+            }
+        }
+    }
+}
