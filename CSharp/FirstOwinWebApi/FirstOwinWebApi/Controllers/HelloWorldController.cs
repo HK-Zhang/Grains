4d7ab0209111dbf9c0c6edb9f541265d1b6c964b
HK Zhang <yxzhk@hotmail.com>
2016-06-26 15:37:33 +0800

    Owin Webapi from Scratch

---
+++ CSharp/FirstOwinWebApi/FirstOwinWebApi/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FirstOwinWebApi.Controllers
+{
+    [RoutePrefix("api/HelloWorld")]
+    public class HelloWorldController : ApiController
+    {
+        [Route("")]
+        public IHttpActionResult Post()
+        {
+
+            return Ok<string>("Hello World");
+
+        }
+
+    }
+}
---
+++ CSharp/FirstOwinWebApi/FirstOwinWebApi/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(FirstOwinWebApi.Startup))]
+
+namespace FirstOwinWebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            //WebApiConfig.Register(config);
+
+            app.UseWebApi(config);
+        }
+    }
+}
