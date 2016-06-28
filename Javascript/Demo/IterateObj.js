93f005c25cfaf596658f0225ba1d58d522e35e45
Zhang HeKe <yxzhk@hotmail.com>
2016-06-28 11:56:09 +0800

    iterate over object's property

---
+++ Javascript/Demo/IterateObj.js
@@ -0,0 +1,22 @@
+﻿function iterate(obj, stack) {
+    for (var property in obj) {
+        if (obj.hasOwnProperty(property)) {
+            if (typeof obj[property] == "object") {
+                iterate(obj[property], stack + property + ".");
+            } else {
+                console.log(stack + property + ":" + obj[property]);
+                //$('#output').append($("<div/>").text(stack + '.' + property))
+            }
+        }
+    }
+}
+
+var example = {
+"prop1": "value1",
+"prop2": ["value2_0", "value2_1"],
+    "prop3": {
+"prop3_1": "value3_1"
+}
+}
+
+iterate(example, '')
