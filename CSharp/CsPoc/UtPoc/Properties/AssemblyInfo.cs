99e00507cb118f540dea755dcf76d5ea643cdbd2
yxzhk <yxzhk@hotmail.com>
2017-07-25 20:38:09 +0800

    add unit test project

---
+++ CSharp/CsPoc/UtPoc/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UtPoc")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UtPoc")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("51ae12c5-0188-4a96-9d07-c237a0045f2c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CSharp/CsPoc/UtPoc/Toolkit/Log4NetDemoTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSDemo.Tests
+{
+    [TestClass()]
+    public class Log4NetDemoTests
+    {
+        [TestMethod()]
+        public void ExecuteTest()
+        {
+            Log4NetDemo t = new Log4NetDemo();
+            t.Execute();
+            Assert.IsTrue(true);
+        }
+    }
+}
