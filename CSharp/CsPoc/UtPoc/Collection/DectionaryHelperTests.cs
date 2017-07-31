01c8b084fb05d3d0284e2d6696d5147c084bc8e4
YXZHK <YXZHK@hotmail.com>
2017-07-31 14:59:01 +0800

    new unit test

---
+++ CSharp/CsPoc/UtPoc/Collection/DectionaryHelperTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CsPoc.Collection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsPoc.Collection.Tests
+{
+    [TestClass()]
+    public class DectionaryHelperTests
+    {
+        [TestMethod()]
+        public void GetValueTest()
+        {
+            Dictionary<object, object> dict = new Dictionary<object, object>();
+            dict.Add("abc", 5);
+            int rst = dict.GetValue<int>("abc");
+            int defaultrst = dict.GetValue<int>("abd");
+            Assert.AreEqual(5, rst);
+            Assert.AreEqual(0, defaultrst);
+        }
+    }
+}
