5f90e4db2a5ebcf95e9d8973fdf88c6d34368f19
YXZHK <YXZHK@hotmail.com>
2017-07-31 14:52:47 +0800

    update

---
+++ CSharp/CsPoc/CsPoc/Collection/DictionaryDemo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsPoc.Collection
+{
+    public class DictionaryDemo
+    {
+
+    }
+
+    public static class DectionaryHelper
+    {
+        public static T GetValue<T>(this IDictionary<object, object> self, string key)
+        {
+            var cnt = self.TryGetValue(key, out object val)
+                ? (T)val
+                : default(T);
+
+            return cnt;
+        }
+    }
+}
