a1e9f0ddec00fc6bcb59f241fff1124be02da183
yxzhk <yxzhk@hotmail.com>
2017-08-03 21:31:32 +0800

    add shouldly for unit test

---
+++ CSharp/CsPoc/CsPoc/Collection/ConcurrentQueueDemo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsPoc.Collection
+{
+    public class ConcurrentQueueDemo
+    {
+        public static ConcurrentQueue<CQDT> queue = new ConcurrentQueue<CQDT>();
+
+        public IEnumerable<CQDT> Dequeue()
+        {
+            CQDT s;
+            List<CQDT> lst = new List<CQDT>();
+
+            do
+            {
+                if (queue.TryDequeue(out s))
+                {
+                    lst.Add(s);
+                }
+                else
+                {
+                    return lst;
+                }
+            } while (true);
+
+        }
+
+    }
+
+    public class CQDT
+    {
+        public string Id { get; set; }
+    }
+}
---
+++ CSharp/CsPoc/UtPoc/Collection/ConcurrentQueueDemoTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CsPoc.Collection;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shouldly;
+
+namespace CsPoc.Collection.Tests
+{
+    [TestClass()]
+    public class ConcurrentQueueDemoTests
+    {
+        [TestMethod()]
+        public void DequeueTest()
+        {
+            var a = new CQDT {Id = "a"};
+            var b = new CQDT {Id = "b"};
+            var c = new CQDT {Id = "c"};
+            var d = new CQDT { Id = "c" };
+
+            ConcurrentQueueDemo.queue.Enqueue(a);
+            ConcurrentQueueDemo.queue.Enqueue(b);
+            ConcurrentQueueDemo.queue.Enqueue(c);
+
+            ConcurrentQueueDemo testObj = new ConcurrentQueueDemo();
+            IEnumerable<CQDT> rst = testObj.Dequeue();
+            a.ShouldBeOneOf(rst.ToArray());
+            b.ShouldBeOneOf(rst.ToArray());
+            c.ShouldBeOneOf(rst.ToArray());
+            d.ShouldBeOneOf(rst.ToArray());
+        }
+    }
+}
