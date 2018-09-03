228af5fdaf4abc5a3f669920a685b41a7f913d38
YXZHK <YXZHK@hotmail.com>
2018-09-03 13:36:39 +0800

    Start EntityFramework Core Journey

---
+++ CSharp/CsPoc/ConsoleApp.EfSql/BloggingContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConsoleApp.EfSql.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp.EfSql
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EfSql;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
---
+++ CSharp/CsPoc/ConsoleApp.EfSql/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp.EfSql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
