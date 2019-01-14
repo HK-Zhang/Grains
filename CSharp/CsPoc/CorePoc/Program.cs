33ea2e4bf3ae408c6c2520b003856663d1672305
yxzhk <yxzhk@hotmail.com>
2019-01-14 20:23:33 +0800

    bufferblock demo

--- CSharp/CsPoc/CorePoc/Program.cs
+++ CSharp/CsPoc/CorePoc/Program.cs
@@ -29,7 +29,7 @@ namespace CorePoc
                 //await WhenAllOrFirstExceptionDemo.ExecuteAsync();
                 //await AsyncCacheDemo.ExecuteAsync();
                 //await AsyncProducerConsumerDemo.ExecuteAsync();
-                //await BufferBlockDemocs.ExecuteAsync();
+                await BufferBlockDemocs.ExecuteAsync();
                 //DataflowBlockCompletition.ExecuteAsync();
                 //await ThrottlingDemo.ExecuteAsync();
                 //await EarlyBailout.ExecuteAsync();
@@ -41,7 +41,7 @@ namespace CorePoc
                 //TransformingManyBlock.ExecuteAsync();
                 //BatchingBlock.ExecuteAsync();
                 //JoiningBlock.ExecuteAsync();
-                BatchedJoiningBlock.ExecuteAsync();
+                //BatchedJoiningBlock.ExecuteAsync();
             }
             catch (Exception ex)
             {
--- CSharp/CsPoc/CorePoc/TAPDemo/BufferBlockDemocs.cs
+++ CSharp/CsPoc/CorePoc/TAPDemo/BufferBlockDemocs.cs
@@ -12,6 +12,12 @@ namespace CorePoc.TAPDemo
 
         public static async Task ExecuteAsync()
         {
+            foo1();
+            //await foo2();
+        }
+
+        static async Task foo2()
+        {
             Produce(1);
             Produce(2);
 
@@ -40,6 +46,101 @@ namespace CorePoc.TAPDemo
         private static void Produce(int data)
         {
             m_data.Post(data);
+        }
+
+
+        // Demonstrates asynchronous dataflow operations.
+        static async Task AsyncSendReceive(BufferBlock<int> bufferBlock)
+        {
+            // Post more messages to the block asynchronously.
+            for (int i = 0; i < 3; i++)
+            {
+                await bufferBlock.SendAsync(i);
+            }
+
+            // Asynchronously receive the messages back from the block.
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(await bufferBlock.ReceiveAsync());
+            }
+
+            /* Output:
+               0
+               1
+               2
+             */
+        }
+
+        static void foo1()
+        {
+            // Create a BufferBlock<int> object.
+            var bufferBlock = new BufferBlock<int>();
+
+            // Post several messages to the block.
+            for (int i = 0; i < 3; i++)
+            {
+                bufferBlock.Post(i);
+            }
+
+            // Receive the messages back from the block.
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(bufferBlock.Receive());
+            }
+
+            /* Output:
+               0
+               1
+               2
+             */
+
+            // Post more messages to the block.
+            for (int i = 0; i < 3; i++)
+            {
+                bufferBlock.Post(i);
+            }
+
+            // Receive the messages back from the block.
+            int value;
+            while (bufferBlock.TryReceive(out value))
+            {
+                Console.WriteLine(value);
+            }
+
+            /* Output:
+               0
+               1
+               2
+             */
+
+            // Write to and read from the message block concurrently.
+            var post01 = Task.Run(() =>
+            {
+                bufferBlock.Post(0);
+                bufferBlock.Post(1);
+            });
+            var receive = Task.Run(() =>
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine(bufferBlock.Receive());
+                }
+            });
+            var post2 = Task.Run(() =>
+            {
+                bufferBlock.Post(2);
+            });
+            Task.WaitAll(post01, receive, post2);
+
+            /* Sample output:
+               2
+               0
+               1
+             */
+
+            // Demonstrate asynchronous dataflow operations.
+            AsyncSendReceive(bufferBlock).Wait();
+
         }
     }
 }
