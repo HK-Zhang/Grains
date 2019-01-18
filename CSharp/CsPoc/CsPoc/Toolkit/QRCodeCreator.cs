a4f68e2c1b4e57b507c41f8a26210f4fb1e44589
YXZHK <YXZHK@hotmail.com>
2019-01-18 16:07:12 +0800

    add icon on QRCode

--- CSharp/CsPoc/CsPoc/Toolkit/QRCodeCreator.cs
+++ CSharp/CsPoc/CsPoc/Toolkit/QRCodeCreator.cs
@@ -22,31 +22,32 @@ namespace CsPoc.Toolkit
             QRCode qrcode = new QRCode(qrCodeData);
 
             // qrcode.GetGraphic 方法可参考最下发“补充说明”
-            Bitmap qrCodeImage = qrcode.GetGraphic(5, Color.Black, Color.White, null, 15, 6, false);
+            Bitmap icon = new Bitmap("favicon.ico");
+            Bitmap qrCodeImage = qrcode.GetGraphic(5, Color.Black, Color.White, icon, 15, 6, false);
             MemoryStream ms = new MemoryStream();
             qrCodeImage.Save(ms, ImageFormat.Jpeg);
 
             // 如果想保存图片 可使用  qrCodeImage.Save(filePath);
-            //            qrCodeImage.Save("1.jpg");
+            qrCodeImage.Save("3.jpg");
             //            // 响应类型
             //            context.Response.ContentType = "image/Jpeg";
             //            //输出字符流
             //            context.Response.BinaryWrite(ms.ToArray());
 
-            using (var srcImage = Image.FromStream(ms))
-            {
-                var newWidth = 83;
-                var newHeight = 83;
-                using (var newImage = new Bitmap(newWidth, newHeight))
-                using (var graphics = Graphics.FromImage(newImage))
-                {
-                    graphics.SmoothingMode = SmoothingMode.AntiAlias;
-                    graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                    graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
-                    graphics.DrawImage(srcImage, new System.Drawing.Rectangle(0, 0, 83, 83));
-                    newImage.Save("2.jpg");
-                }
-            }
+//            using (var srcImage = Image.FromStream(ms))
+//            {
+//                var newWidth = 83;
+//                var newHeight = 83;
+//                using (var newImage = new Bitmap(newWidth, newHeight))
+//                using (var graphics = Graphics.FromImage(newImage))
+//                {
+//                    graphics.SmoothingMode = SmoothingMode.AntiAlias;
+//                    graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+//                    graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+//                    graphics.DrawImage(srcImage, new System.Drawing.Rectangle(0, 0, 83, 83));
+//                    newImage.Save("2.jpg");
+//                }
+//            }
         }
 
 
