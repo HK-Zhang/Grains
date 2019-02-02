712d148cb48050a68c99cd987df7afb94ab213a9
yxzhk <yxzhk@hotmail.com>
2019-02-02 21:05:39 +0800

    ASP.NET Identity Stripped Bare

---
+++ CSharp/CsPoc/AspIdentity/Controllers/AuthController.cs
@@ -0,0 +1,75 @@
+﻿using AspIdentity.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspIdentity.Controllers
+{
+    [AllowAnonymous]
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public ActionResult LogIn(string returnUrl)
+        {
+            var model = new LogInModel
+            {
+                ReturnUrl = returnUrl
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult LogIn(LogInModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            // Don't do this in production!
+            if (model.Email == "admin@admin.com" && model.Password == "password")
+            {
+                var identity = new ClaimsIdentity(new[] {
+                new Claim(ClaimTypes.Name, "Ben"),
+                new Claim(ClaimTypes.Email, "a@b.com"),
+                new Claim(ClaimTypes.Country, "England")
+            },
+                    "ApplicationCookie");
+
+                var ctx = Request.GetOwinContext();
+                var authManager = ctx.Authentication;
+
+                authManager.SignIn(identity);
+
+                return Redirect(GetRedirectUrl(model.ReturnUrl));
+            }
+
+            // user authN failed
+            ModelState.AddModelError("", "Invalid email or password");
+            return View();
+        }
+
+        private string GetRedirectUrl(string returnUrl)
+        {
+            if (string.IsNullOrEmpty(returnUrl) || !Url.IsLocalUrl(returnUrl))
+            {
+                return Url.Action("index", "home");
+            }
+
+            return returnUrl;
+        }
+
+        public ActionResult LogOut()
+        {
+            var ctx = Request.GetOwinContext();
+            var authManager = ctx.Authentication;
+
+            authManager.SignOut("ApplicationCookie");
+            return RedirectToAction("index", "home");
+        }
+    }
+}
---
+++ CSharp/CsPoc/AspIdentity/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspIdentity.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var claimsIdentity = User.Identity as ClaimsIdentity;
+            ViewBag.Country = claimsIdentity.FindFirst(ClaimTypes.Country).Value;
+
+            return View();
+        }
+    }
+}
