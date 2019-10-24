3bffb472ff9cba7df44a05d0acf065dfafd71324
He Ke Henry Zhang <He.Ke.Henry.Zhang@dnvgl.com>
2019-10-24 09:46:58 +0800

    test application insight performance

---
+++ CSharp/CsPoc/PerfCounter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.Extensibility.PerfCounterCollector;
+
+namespace PerfCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TelemetryConfiguration.Active.InstrumentationKey = "to be replaced";
+            _ = TelemetryConfiguration.Active;
+            
+            TelemetryConfiguration configuration = TelemetryConfiguration.CreateDefault();
+            configuration.InstrumentationKey = "to be replaced";
+            var telemetryClient = new TelemetryClient(configuration);
+            telemetryClient.TrackTrace("Hello World5!");
+            //
+            //            var perfCollectorModule = new PerformanceCollectorModule();
+            ////            perfCollectorModule.Counters.Add(new PerformanceCounterCollectionRequest(
+            ////                @"\Memory\Available Bytes", "Available Bytes"));
+            //            perfCollectorModule.Initialize(configuration);
+
+            telemetryClient.TrackTrace("Hello World6!");
+
+            telemetryClient.Flush();
+            Console.WriteLine("Hello World6!");
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+                telemetryClient.Flush();
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharp/CsPoc/PerformanceCounter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.Extensibility.PerfCounterCollector;
+
+namespace PerformanceCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TelemetryConfiguration configuration = TelemetryConfiguration.CreateDefault();
+            configuration.InstrumentationKey = "replace with real value";
+
+
+            var perfCollectorModule = new PerformanceCollectorModule();
+            perfCollectorModule.Counters.Add(new PerformanceCounterCollectionRequest(
+                @"\Memory\Available Bytes", "Available Bytes"));
+            perfCollectorModule.Initialize(configuration);
+
+            var telemetryClient = new TelemetryClient(configuration);
+            telemetryClient.TrackTrace("Hello World4!");
+
+            Console.WriteLine("Hello World2!");
+            Console.ReadKey();
+        }
+    }
+}
